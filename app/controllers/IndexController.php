3866f885e774604472eca2e52dbdfc225c5fb151
carollai <carollai945@gmail.com>
2020-11-17 12:29:19 +0800

    update code

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -13,17 +13,7 @@ class IndexController extends Controller
     
 	public function report1Action()
 	{
-        //只要加上$this->view->message ，程式會自動抓取app/views/scripts/controller相同名稱資料夾/action相同的檔名.phml
-        //例如app/views/scripts/index/report1.phtml
 		$this->view->message = "";
-        
-        //透過models的物件，讀取資料庫資料
-        $borrowing = new Borrowing();
-        $data = $borrowing->getReport1();
-        
-        //設定app/views/scripts/index/report1.phtml需要的變數，phtml檔案的用法 $this->_data['reportData'];
-        $this->view->__set('reportData',$data);
-
 	}
     
 	public function borrowingAction()
@@ -61,7 +51,7 @@ class IndexController extends Controller
         
         $borrowing = new Borrowing();
         $data = $borrowing->getReport1();
-
+        
         $this->view->renderJson($data);
     }
 }
